01fc75d346f551d0a6e162320b02d9169419eaf3
Shubh Engineer <itssmagic321@gmail.com>
2024-07-11 02:08:44 +0530

    db.js in database folder is modified

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config;
+dotenv.config();
 
 const DBConnection = async() =>{
    const MONGODB_URL = process.env.MONGODB_URL;
    try {
-        await mongoose.connect(MONGODB_URL,{userNewUrlParser:true});
+        await mongoose.connect(MONGODB_URL,{useNewUrlParser:true});
         console.log("DB connection established");
     
    } catch (error) {
