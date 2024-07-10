0e615312990070e117e44702301df5261f4517c5
Shubh Engineer <itssmagic321@gmail.com>
2024-07-11 01:44:55 +0530

    modified db.js in backend folder

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config;
+
+const DBConnection = async() =>{
+   const MONGODB_URL = process.env.MONGODB_URL;
+   try {
+        await mongoose.connect(MONGODB_URL,{userNewUrlParser:true});
+        console.log("DB connection established");
+    
+   } catch (error) {
+        console.log("error connection to mongodb: " + error);
+   }
+}
+module.exports = {DBConnection};
