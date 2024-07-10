485ce39421ad5ad29043d72b31529670abafafad
Shubh Engineer <itssmagic321@gmail.com>
2024-07-11 00:50:38 +0530

    index.js is modified

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,21 @@
-const express = requrire('express');
+const express = require('express');
 const app = express();
+
+//server
+app.post("/register",(req,body) => {
+    try {
+        
+    } catch (error) {
+        const {firstname, lastname, email, password} = req.body;
+
+        if(!(firstname && lastname && email && password))
+        {
+            return res.status(400).send("please fill all the details!");
+        }
+    }
+  });
+
+//server
+app.listen(8000,() => {
+  console.log("Server is listening to port 8000");
+});
