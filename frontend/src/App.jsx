14f60da6c5f15f61f16159c3815831b0d7a7adbd
Shubh Engineer <itssmagic321@gmail.com>
2024-07-17 01:27:49 +0530

    modified

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Register from './components/Register';
+// import Login from './components/Login';
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Register/>
+     {/* <Login/> */}
+    </>
+  )
+}
+
+export default App
