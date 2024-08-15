9f9aab881ac9927f83b6e10e093b724a84ef522a
Shubh Engineer <itssmagic321@gmail.com>
2024-08-15 12:22:18 +0530

    add base url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.VITE_BASE_URL || 'http://localhost:8000',
     headers: {
         
         'Content-Type': 'application/json',
